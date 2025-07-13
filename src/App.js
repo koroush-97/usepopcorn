acd0db0089681a5b2f53783fe458cab941d1b0e0
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-07-13 17:25:09 +0330

    numResult bug fixed

--- src/App.js
+++ src/App.js
@@ -55,18 +55,18 @@ export default function App() {
 
   return (
     <>
-      <NavBar />
+      <NavBar movies={movies} />
       <Main movies={movies} />
     </>
   );
 }
 
-function NavBar() {
+function NavBar({ movies }) {
   return (
     <nav className="nav-bar">
       <Logo />
       <Search />
-      <NumResults />
+      <NumResults movies={movies} />
     </nav>
   );
 }
@@ -80,10 +80,10 @@ function Logo() {
   );
 }
 
-function NumResults() {
+function NumResults({ movies }) {
   return (
     <p className="num-results">
-      Found <strong>X</strong> results
+      Found <strong>{movies.length}</strong> results
     </p>
   );
 }
