8f72dcc03a89953c2b893e62ad44e25577fdf75e
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-07-14 20:32:48 +0330

    StarRating ternery has been edit

--- src/StarRating.js
+++ src/StarRating.js
@@ -47,7 +47,7 @@ export default function StarRating({
       </div>
       <p style={textStyle}>
         {messages.length === maxRating
-          ? messages[rating - 1]
+          ? messages[tempRating ? tempRating - 1 : rating - 1]
           : tempRating || rating || " "}
       </p>
     </div>
