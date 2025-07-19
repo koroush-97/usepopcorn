c21274be7fc5f6ab8dd8d714b931a0502fdabcea
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-07-19 17:08:01 +0330

    fixed sideEffect of app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
+import "./index.css";
+import App from "./App";
 import StarRating from "./StarRating";
 
 function Test() {
@@ -17,11 +17,11 @@ function Test() {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
+    <App />
+    {/* <StarRating
       maxRating={5}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-    />
+    /> */}
     <StarRating maxRating={5} color="red" defaultRating={3} />
     <Test />
   </React.StrictMode>
