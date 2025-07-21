e2a9a34d81e44c4d530b3f6481ca579a3ce66cef
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-07-21 17:01:44 +0330

    change  inital state for selectedID from hardcode to dynamic

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ export default function App() {
   const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
-  const [selectedID, setSelectedID] = useState("tt0339279");
+  const [selectedID, setSelectedID] = useState(null);
 
   // const tempQuery = "interstellar";
   useEffect(
