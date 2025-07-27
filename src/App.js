3b2aa318e570a220a92c16fa0d85822f85f83ca2
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-07-27 12:23:25 +0330

    add cleancode phase for title

--- src/App.js
+++ src/App.js
@@ -340,6 +340,10 @@ function MovieDetails({ selectedID, onCloseMovie, onAddWatched, watched }) {
     function () {
       if (!title) return;
       document.title = `Movie | ${title}`;
+
+      return function () {
+        document.title = "usePopcorn";
+      };
     },
     [title]
   );
