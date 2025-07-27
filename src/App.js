b787421041357a7890376a18622d093a46fd2091
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-07-27 13:10:09 +0330

    set name for cleaning function

--- src/App.js
+++ src/App.js
@@ -325,11 +325,17 @@ function MovieDetails({ selectedID, onCloseMovie, onAddWatched, watched }) {
 
   useEffect(
     function () {
-      document.addEventListener("keydown", function (e) {
+      function callback(e) {
         if (e.code === "Escape") {
           onCloseMovie();
         }
-      });
+      }
+
+      document.addEventListener("keydown", callback);
+
+      return function () {
+        document.removeEventListener("keydown", callback);
+      };
     },
     [onCloseMovie]
   );
