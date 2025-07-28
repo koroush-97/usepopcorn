2a307fe61382b1bfccb58b6bb5191edbe435174e
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-07-28 15:18:42 +0330

    deactivate state of watched movie

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ const KEY = "8a3bec74";
 export default function App() {
   const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const [selectedID, setSelectedID] = useState(null);
+  // const [watched, setWatched] = useState([]);
 
   // const tempQuery = "interstellar";
 
@@ -33,13 +33,6 @@ export default function App() {
   function handleDeleteWatch(id) {
     setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
   }
-
-  useEffect(
-    function () {
-      localStorage.setItem("watched", JSON.stringify(watched));
-    },
-    [watched]
-  );
 
   useEffect(
     function () {
