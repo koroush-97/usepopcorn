42a3aba51b43afe32d368582b2534c35f31723d5
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-07-28 15:33:19 +0330

    Remove line for fix bug

--- src/App.js
+++ src/App.js
@@ -30,8 +30,6 @@ export default function App() {
 
   function handleAddWatched(movie) {
     setWatched((watched) => [...watched, movie]);
-
-    localStorage.setItem("watched", JSON.stringify([...watched, movie]));
   }
 
   function handleDeleteWatch(id) {
