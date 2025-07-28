7ab81bc74b4c1f04a66c9f9ada3327fca2f2dbe3
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-07-28 15:24:06 +0330

    change data from localstorage to string

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ export default function App() {
   // const [watched, setWatched] = useState([]);
   const [watched, setWatched] = useState(function () {
     const storedValue = localStorage.getItem("watched");
-    return storedValue;
+    return JSON.parse(storedValue);
   });
 
   // const tempQuery = "interstellar";
