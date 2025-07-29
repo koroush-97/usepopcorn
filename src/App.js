5cd93dadc58bf8120a3726555187b366d9067524
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-07-29 16:03:10 +0330

    fix bug of delete from localstorage

--- src/App.js
+++ src/App.js
@@ -30,11 +30,20 @@ export default function App() {
 
   function handleAddWatched(movie) {
     setWatched((watched) => [...watched, movie]);
+
+    // localStorage.setItem("watched", JSON.stringify([...watched, movie]));
   }
 
   function handleDeleteWatch(id) {
     setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
   }
+
+  useEffect(
+    function () {
+      localStorage.setItem("watched", JSON.stringify(watched));
+    },
+    [watched]
+  );
 
   useEffect(
     function () {
