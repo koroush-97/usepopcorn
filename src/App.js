1292ec1820a05ec1e0d5aa1473af77bdc265f2ae
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-08-02 11:43:18 +0330

    set customHooks: useMovie

--- src/App.js
+++ src/App.js
@@ -45,53 +45,6 @@ export default function App() {
     [watched]
   );
 
-  useEffect(
-    function () {
-      const controller = new AbortController();
-      async function fetchMovies() {
-        try {
-          setIsLoading(true);
-          setError("");
-          const res = await fetch(
-            `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
-            { signal: controller.signal }
-          );
-
-          if (!res.ok) {
-            throw new Error("Something went wrong with fetching movies");
-          }
-
-          const data = await res.json();
-
-          if (data.Response === "False") {
-            throw new Error("movie not found");
-          }
-
-          setMovies(data.Search);
-        } catch (err) {
-          console.error("Fetch error:", err.message);
-          setError("Movie not found");
-        } finally {
-          setIsLoading(false);
-        }
-      }
-
-      if (query.length < 3) {
-        setMovies([]);
-        setError("movie not Found");
-        return;
-      }
-
-      handleCloseMovie();
-
-      fetchMovies();
-      return function () {
-        controller.abort();
-      };
-    },
-    [query]
-  );
-
   return (
     <>
       <NavBar>
