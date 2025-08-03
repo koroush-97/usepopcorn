f054b97907016f0ac67cdeb849ce2c5865f6023e
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-08-03 11:00:27 +0330

    create body of uselocalstoragestate

--- src/hooks/useLocalstorageState.js
+++ src/hooks/useLocalstorageState.js
@@ -1,1 +1,8 @@
-export function useLocalstorageState() {}
+import { useState } from "react";
+
+export function useLocalstorageState(initialState) {
+  const [watched, setWatched] = useState(function () {
+    const storedValue = localStorage.getItem("watched");
+    return JSON.parse(storedValue);
+  });
+}
